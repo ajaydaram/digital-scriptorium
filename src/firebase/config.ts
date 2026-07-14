385866178d89c9687d0da65c58a6d3ce3d0d513b
Ajay Daram <ajayforchrist777@gmail.com>
2026-07-14 11:40:56 +0800

    Fix security vulnerability: remove hardcoded Firebase API key from config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7700598443-b2b6f",
-  "appId": "1:475637894992:web:69ca98310de4289c46e655",
-  "apiKey": "AIzaSyBDNTrGvBZkC2GTDsn04_qz5CdR8RPeL0g",
-  "authDomain": "studio-7700598443-b2b6f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "475637894992"
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-7700598443-b2b6f",
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:475637894992:web:69ca98310de4289c46e655",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-7700598443-b2b6f.firebaseapp.com",
+  "measurementId": process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "475637894992"
 };
